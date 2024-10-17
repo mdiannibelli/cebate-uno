c3e963ece046f983dbfe767316bebb9302bdc075
Marcos <mdiannibelli@gmail.com>
2024-10-17 19:54:35 -0300

    fix(-):cloudinary pictures

--- frontend/src/components/Bombillas/BombillasGrid.tsx
+++ frontend/src/components/Bombillas/BombillasGrid.tsx
@@ -5,14 +5,14 @@ export const BombillasGrid = () => {
     //const { bombillas } = useBombillas();
     const { filteredBombillas } = useFilters();
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 max-w-[1296px] mx-auto mt-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 max-w-[1296px] mx-auto mt-8 gap-8">
             {
                 filteredBombillas.map((bombilla) => (
                     <div key={bombilla.id} className="flex flex-col mt-2 justify-center items-center my-2" >
                         <figure>
                             <img src={bombilla.img} alt={bombilla.productName} className="xl:w-[330px] xl:h-[300px] size-64" />
                         </figure>
-                        <h3 className="font-light text-center uppercase text-xl mt-1">{bombilla.productName}</h3>
+                        <h3 className="font-light text-center uppercase text-lg max-w-[280px] mt-4">{bombilla.productName}</h3>
                         <aside className="flex gap-x-4 items-center justify-center mt-2">
                             <span className="font-regular text-center uppercase text-lg">${Number(bombilla.price).toLocaleString()}</span>
                             <button><MdAddShoppingCart className="size-4" /></button>
--- frontend/src/components/Mates/MatesGrid.tsx
+++ frontend/src/components/Mates/MatesGrid.tsx
@@ -6,14 +6,14 @@ const MatesGrid = () => {
   //const { mates } = useMates();
   const { filteredMates } = useFilters()
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 max-w-[1296px] mx-auto mt-8">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 max-w-[1296px] mx-auto mt-8 gap-8">
       {
         filteredMates.map((mate) => (
           <div key={mate.id} className="flex flex-col mt-2 justify-center items-center my-2" >
             <figure>
               <img src={mate.img} alt={mate.productName} className="xl:w-[330px] xl:h-[300px] size-64" />
             </figure>
-            <h3 className="font-light text-center uppercase text-xl mt-1">{mate.productName}</h3>
+            <h3 className="font-light text-center uppercase text-lg max-w-[280px] mt-4">{mate.productName}</h3>
             <aside className="flex gap-x-4 items-center justify-center mt-2">
               <span className="font-regular text-center uppercase text-lg">${Number(mate.price).toLocaleString()}</span>
               <button><MdAddShoppingCart className="size-4" /></button>
