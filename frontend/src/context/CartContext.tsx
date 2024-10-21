63e72adb82e42aecbe3d0b4f8e5ab7573ff8fbe6
Marcos <mdiannibelli@gmail.com>
2024-10-21 13:03:44 -0300

    add(-):localStorage

--- frontend/src/context/CartContext.tsx
+++ frontend/src/context/CartContext.tsx
@@ -1,5 +1,5 @@
 import { BombillaType, MatesType } from '@/types'
-import React, { createContext, useState } from 'react'
+import React, { createContext, useEffect, useState } from 'react'
 
 interface CartContextType {
     cart: CartType[]
@@ -15,9 +15,25 @@ type CartType = MatesType | BombillaType
 export const CartContext = createContext<CartContextType | undefined>(undefined);
 
 export default function CartProvider({ children }: { children: React.ReactNode }) {
-    const [cart, setCart] = useState<CartType[]>([]);
-    const [totalAmount, setTotalAmount] = useState(0);
-    const [totalItems, setTotalItems] = useState(0);
+    const [cart, setCart] = useState<CartType[]>(() => {
+        const storedCart = localStorage.getItem('cart');
+        return storedCart ? JSON.parse(storedCart) : []
+    });
+    const [totalAmount, setTotalAmount] = useState<number>(() => {
+        const storedTotalAmount = localStorage.getItem('totalAmount');
+        return storedTotalAmount ? JSON.parse(storedTotalAmount) : 0
+    });
+    const [totalItems, setTotalItems] = useState<number>(() => {
+        const storedTotalItems = localStorage.getItem('totalItems');
+        return storedTotalItems ? JSON.parse(storedTotalItems) : 0
+    });
+
+    // Save Local Storage
+    useEffect(() => {
+        localStorage.setItem('cart', JSON.stringify(cart));
+        localStorage.setItem('totalAmount', JSON.stringify(totalAmount));
+        localStorage.setItem('totalItems', JSON.stringify(totalItems));
+    }, [cart, totalAmount, totalItems]);
 
     function addItemToCart(item: MatesType | BombillaType) {
         const itemToFind = cart.findIndex(cartItem => cartItem.id === item.id);
