4cef4ed22348f7ec0a7f781c3c0f20be11ffbdec
Marcos <mdiannibelli@gmail.com>
2024-10-21 14:36:24 -0300

    update(-):api_url

--- frontend/src/config/api.config.ts
+++ frontend/src/config/api.config.ts
@@ -1,3 +1,3 @@
-export const API_URL = import.meta.env.VITE_SERVER_API ?? 'http://localhost:3000'
+export const API_URL = import.meta.env.VITE_SERVER_API /* || 'http://localhost:3000' */
 export const MATES_ENDPOINT = `${API_URL}/api/mates`
 export const BOMBILLAS_ENDPOINT = `${API_URL}/api/bombillas`
